47c3954b1eb655f0130c1c339ca48df98bdf4c71
NoorSulaiman <noor.s.suliman@gmail.com>
2018-06-09 15:01:59 +0300

    user sign up

--- src/models/User.js
+++ src/models/User.js
@@ -2,12 +2,14 @@ import mongoose from 'mongoose';
 import bcrypt from 'bcrypt';
 import jwt from 'jsonwebtoken';
 import dotenv from 'dotenv';
+import uniqueValidator from 'mongoose-unique-validator';
 // add uniqness and email validations 
 
 dotenv.config()
 const schema = new mongoose.Schema({
-    email: { type: String, required: true, lowercase: true },
-    passwordHash: { type: String, required: true }
+    email: { type: String, required: true, lowercase: true, unique: true },
+    passwordHash: { type: String, required: true },
+    confirmed: { type: Boolean, default: false }
 },
     { timestampes: true }
 );
@@ -15,6 +17,10 @@ const schema = new mongoose.Schema({
 schema.methods.isValidPassword = function isValidPassword(password) {
     return bcrypt.compare(password, this.passwordHash);
 };
+
+schema.methods.setPassword = function setPassword(password) {
+    this.passwordHash = bcrypt.hashSync(password, 10)
+}
 
 schema.methods.generateJWT = function generateJWT() {
     return jwt.sign(
@@ -26,8 +32,12 @@ schema.methods.generateJWT = function generateJWT() {
 schema.methods.toAuthJSON = function toAuthJSON() {
     return {
         email: this.email,
+        confirmed: this.confirmed,
         token: this.generateJWT()
+
     }
 }
+
+schema.plugin(uniqueValidator, { message: "This email is already taken" })
 
 export default mongoose.model('User', schema);
